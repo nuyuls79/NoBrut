3ea6400d98520c0feff88ede02d8b6f647c23246
nuyuls79 <nuyulshare001@gmail.com>
2026-05-03 13:44:51 +0700

    Enhance readRaw function with exception handling
    Add error handling for reading raw resources.

--- app/src/main/java/com/ycngmn/notubetv/utils/readRaw.kt
+++ app/src/main/java/com/ycngmn/notubetv/utils/readRaw.kt
@@ -1,8 +1,19 @@
 package com.ycngmn.notubetv.utils
 
 import android.content.Context
+import java.io.IOException
 
 fun readRaw(context: Context, resId: Int): String {
-    return context.resources.openRawResource(resId)
-        .bufferedReader().use { it.readText() }
+
+    return try {
+
+        context.resources.openRawResource(resId)
+            .bufferedReader()
+            .use { it.readText() }
+
+    } catch (e: IOException) {
+        "" // fallback aman
+    } catch (e: Exception) {
+        "" // fallback untuk semua error lain
+    }
 }
